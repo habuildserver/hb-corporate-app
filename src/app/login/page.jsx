a6a2ff0597db8630f3c800e73843290af74c9a02
habuildserver <115799057+habuildserver@users.noreply.github.com>
2024-02-01 11:26:43 +0530

    task/HAB-1089: Added Show password feature for password input and cursor for username input

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 import { apiHelper } from "@/lib/apiClient";
 import { LoginApis } from "@/services/generalApis";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL;
 
 const Login = () => {
@@ -13,6 +14,7 @@ const Login = () => {
   const [corporateDetails, setCorporateDetails] = useState({});
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const [showPassword, setshowpassword] = useState(true);
 
   const login = async () => {
     if (!email && !password) {
@@ -35,7 +37,6 @@ const Login = () => {
     let result = await apiHelper(LoginApis.LOGIN(), "POST", inputData);
 
     if (Object.keys(result.success).length != 0) {
-      
       const companyData = JSON.stringify(result.success);
       localStorage.setItem("companyData", companyData);
       router.push("/dashboard");
@@ -46,6 +47,12 @@ const Login = () => {
     }
     setLoading(false);
   };
+
+  const emailInputRef = useRef(null);
+  useEffect(() => {
+    emailInputRef.current.focus();
+  }, []);
+
 
   return (
     <>
@@ -60,49 +67,61 @@ const Login = () => {
           />
           <p className="text-[18px] font-bold pb-5">Sign In to Your Account</p>
           <div className="flex flex-col gap-9">
-            <label>
+            <label className="">
               <p className="text-[14px] mb-2">Email</p>
               <input
+          ref={emailInputRef} // Assign the ref to the email input
+
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 required
                 placeholder="Enter Your Email"
-                className="px-5 py-2 border border-[#E2E8F0] rounded-lg bg-[#394150]"
+                className="px-5 py-2 border border-[#E2E8F0] rounded-lg bg-[#394150] w-full"
               />
             </label>
-            <label>
+            <label className="">
               <p className="text-[14px] mb-2">Password</p>
-              <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                placeholder="Enter Your Password"
-                className="px-5 py-2 border border-[#E2E8F0] rounded-lg bg-[#394150]"
-              />
+              <div className="flex justify-center items-center border border-[#E2E8F0] rounded-lg">
+                <input
+                  type={showPassword ? "password" : "text"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  placeholder="Enter Your Password"
+                  className="px-5 py-2 bg-[#394150] rounded-lg w-full focus:outline-none"
+                />
+                <div onClick={() => setshowpassword(!showPassword)} className="cursor-pointer">
+                  {showPassword ? (
+                    <EyeOffIcon className="flex-shrink-0 mx-1" />
+                  ) : (
+                    <EyeIcon className="flex-shrink-0 mx-1" />
+                  )}
+                </div>
+              </div>
             </label>
             <div>
-
-              {loading?
-              <>
-              <p className="bg-[#176FC1] p-2 text-center rounded-lg w-full text-white text-[10px] font-bold">
-              <div className="flex justify-center  items-center">
-                    <div
-                      className="animate-spin rounded-full h-5 w-5 border-[#fff] mr-2"
-                      style={{
-                        borderTop: "2px solid #fff",
-                      }}
-                    ></div>
-                  </div>
-              </p>
-              </>:
-              <button
-                className="bg-[#176FC1] p-2 text-center rounded-lg w-full text-white text-[10px] font-bold"
-                onClick={() => {
-                  login();
-                }}
-              >
-                SIGN IN
-              </button>}
+              {loading ? (
+                <>
+                  <p className="bg-[#176FC1] p-2 text-center rounded-lg w-full text-white text-[10px] font-bold">
+                    <div className="flex justify-center  items-center">
+                      <div
+                        className="animate-spin rounded-full h-5 w-5 border-[#fff] mr-2"
+                        style={{
+                          borderTop: "2px solid #fff",
+                        }}
+                      ></div>
+                    </div>
+                  </p>
+                </>
+              ) : (
+                <button
+                  className="bg-[#176FC1] p-2 text-center rounded-lg w-full text-white text-[10px] font-bold"
+                  onClick={() => {
+                    login();
+                  }}
+                >
+                  SIGN IN
+                </button>
+              )}
             </div>
           </div>
         </div>
