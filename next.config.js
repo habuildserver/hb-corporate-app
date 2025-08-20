589f53bd879b4c22656fd3541316ac8e9932bf94
Tanmay <tanmay@Tanmays-MacBook-Air-2.local>
2025-08-20 13:03:31 +0530

    Enhance security headers in Next.js configuration and maintain environment variables

--- next.config.js
+++ next.config.js
@@ -1,23 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-}
-
-module.exports = nextConfig;
-module.exports = {
-    env: {
-        API_BASE_URL: process.env.API_BASE_URL,
-        IMAGE_BASE_URL: process.env.IMAGE_BASE_URL,
-    },
-    images: {
-        remotePatterns: [
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: '/(.*)',
+        headers: [
           {
-            protocol: "https",
-            hostname:
-              "assets.habit.yoga",
-            port: "",
-            pathname: "/corporateassets/**",
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
           },
         ],
       },
-}
+    ];
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    IMAGE_BASE_URL: process.env.IMAGE_BASE_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.habit.yoga",
+        port: "",
+        pathname: "/corporateassets/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
